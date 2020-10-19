0124ad4434cd3dc236659530a8428984f3fd08e2
Vlad Dr.Frame <remenjuk2010@gmail.com>
2020-10-19 16:54:20 +0300

    script for add links class

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -5,5 +5,13 @@ $(document).ready(function () {
   $('#close').on('click', function () {
     $('#menu-mobile').addClass('is-hiden');
   });
+
+  $('.header__navigation-link').on('click', function () {
+    $(this).addClass('header__navigation-link--active');
+    $('.header__navigation-link').not(this).removeClass('header__navigation-link--active');
+    $('#menu-mobile').addClass('is-hiden');
+  })
+
+
 });
 
