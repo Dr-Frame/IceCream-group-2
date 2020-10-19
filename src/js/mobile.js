86b18b8c10c8e406cc58d8af74a0c8bb910238b7
Vlad Dr.Frame <remenjuk2010@gmail.com>
2020-10-19 17:52:38 +0300

    script for links added

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -10,7 +10,13 @@ $(document).ready(function () {
     $(this).addClass('header__navigation-link--active');
     $('.header__navigation-link').not(this).removeClass('header__navigation-link--active');
     $('#menu-mobile').addClass('is-hiden');
+    $('.back').removeClass('visually-hidden');
   })
+  $('.back').on('click', function () {
+    $('.back').addClass('visually-hidden');
+    $('#home').addClass('header__navigation-link--active');
+    $('.header__navigation-link').not('#home').removeClass('header__navigation-link--active');
+  });
 
 
 });
